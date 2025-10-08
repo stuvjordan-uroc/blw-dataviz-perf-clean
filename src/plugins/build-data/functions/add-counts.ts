3e28e24c1b13a17caa1d55b6e5d25d79f6c1218c
stu <stuvjordan@gmail.com>
2025-10-08 12:54:31 -0400

    re-drafted addCounts so that it mutates the responses objects, instead of making and returning shallow copies

--- src/plugins/build-data/functions/add-counts.ts
+++ src/plugins/build-data/functions/add-counts.ts
@@ -1,21 +1,27 @@
 import type { Response } from "../../../assets/raw-data/meta-types";
+//Note: This function mutates the objects in the 
+//responses array that you pass to it!
 export function addCounts(
   responses: Response[],
   sampleSize: number
-): Response & { count: number } {
-  type WithCount = Response & { count: number, floatCount: number }
-  const withCounts: WithCount[] = responses.map((response) => ({
-    ...response,
-    count: Math.floor(response.proportion * sampleSize),
-    floatCount: response.proportion * sampleSize
-  }))
-  while (withCounts.reduce((acc: number, curr) => acc + curr.count, 0) < sampleSize) {
-    const farthest = withCounts.reduce(((acc: WithCount, curr) =>
+): (Response & { count: number })[] {
+  type WithCounts = Response & { count: number, floatCount: number }
+  //add the floatCount and count to each response object.
+  responses.forEach((response) => {
+    const floatCount = response.proportion * sampleSize;
+    (response as WithCounts).floatCount = floatCount;
+    (response as WithCounts).count = Math.floor(floatCount);
+  })
+  //adjust the counts upward until we get to the sample size
+  while (responses.reduce((acc: number, curr) => acc + (curr as WithCounts).count, 0) < sampleSize) {
+    const farthest = (responses as WithCounts[]).reduce(((acc: WithCounts, curr: WithCounts) =>
       (acc.floatCount - acc.count > curr.floatCount - curr.count) ? acc : curr
     ))
     farthest.count = farthest.count + 1
   }
-  return withCounts.map((response) => Object.fromEntries(
-    Object.entries(response).filter(([key, _val]) => key !== "floatCount")
-  ))
+  //remove the float counts
+  (responses as WithCounts[]).forEach((response) => {
+    delete (response as Response & { count: number, floatCount?: number }).floatCount
+  })
+  return responses as (Response & { count: number })[]
 }
