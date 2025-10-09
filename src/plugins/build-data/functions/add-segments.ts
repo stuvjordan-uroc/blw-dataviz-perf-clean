2d0c173dba66eb434b0e0f6f53d0c4a8beaf2710
stu <stuvjordan@gmail.com>
2025-10-09 10:56:57 -0400

    addSegments draft done.  Need to modify so it doesn't mutate meta objects.

--- src/plugins/build-data/functions/add-segments.ts
+++ src/plugins/build-data/functions/add-segments.ts
@@ -1,64 +1,99 @@
 import type { Layout } from "../../../assets/config/config-types";
 import type { Response } from "../../../assets/raw-data/meta-types";
 import type { PointPosition } from "./point-positions";
-import { pointPositions, pointPositions } from "./point-positions";
+import { pointPositions } from "./point-positions";
 
 interface Segment {
   topLeftY: number,
   topLeftX: number,
   height: number,
   width: number,
-  pointPositions: PointPosition[]
+  pointPositions: PointPosition[],
+  error: boolean
 }
 
 type ResponseWithSegment = Response & { count: number } & { segment: Segment }
 
-function addSegments(
-  wave: number | null,
-  party: string[] | null,
+interface SegmentGroup {
+  topLeftX: number,
+  topLeftY: number,
+  widthToBeDistributed: number,
+  height: number
+}
+
+function segmentGroup(
+  wave: { index: number, value: number } | null,
+  party: { index: number, value: string[] } | null,
+  responseArrayLength: number,
+  layout: Layout,
+  numWaves: number,
+  numPartyGroups: number
+): SegmentGroup {
+  const vizHeight = numWaves * (layout.labelHeight + layout.waveHeight)
+  const totalGroupWidth = (party === null) ? layout.vizWidth : (layout.vizWidth - layout.partyGap * (numPartyGroups - 1)) / numPartyGroups
+  return ({
+    topLeftX: (party === null) ? 0 : party.index * (totalGroupWidth + layout.partyGap),
+    topLeftY: layout.labelHeight + ((wave === null) ? 0 : wave.index * (layout.labelHeight + layout.waveHeight)),
+    widthToBeDistributed: totalGroupWidth - layout.responseGap * (responseArrayLength - 1),
+    height: (wave === null) ? vizHeight - layout.labelHeight : layout.waveHeight
+  })
+}
+
+function segments(
+  responses: (Response & { count: number })[],
+  segmentGroup: SegmentGroup,
+  responseGap: number,
+  pointRadius: number
+): ResponseWithSegment[] {
+  responses.forEach((response, responseIdx) => {
+    const responseTopLeftX = responses.slice(0, responseIdx).reduce((acc: number, curr) =>
+      acc + responseGap + curr.proportion * segmentGroup.widthToBeDistributed,
+      segmentGroup.topLeftX
+    )
+    const responseWidth = segmentGroup.widthToBeDistributed * response.proportion;
+    const positions = pointPositions(
+      segmentGroup.topLeftX,
+      segmentGroup.topLeftY,
+      responseWidth,
+      segmentGroup.height,
+      response.count,
+      pointRadius
+    );
+    (response as ResponseWithSegment).segment = {
+      topLeftX: responseTopLeftX,
+      topLeftY: segmentGroup.topLeftY,
+      width: responseWidth,
+      height: segmentGroup.height,
+      pointPositions: positions.data,
+      error: positions.error
+    }
+  })
+  return responses as ResponseWithSegment[]
+}
+
+
+
+export function addSegments(
+  wave: { index: number, value: number } | null,
+  party: { index: number, value: string[] } | null,
   responses: (Response & { count: number })[],
   layout: Layout,
   numWaves: number,
   partyResponseGroups: string[][]
 ): ResponseWithSegment[] {
-  if (wave === null) {
-    //not split-by-wave
-    const segmentHeight = (layout.labelHeight + layout.waveHeight) * numWaves
-    const topLeftY = layout.labelHeight / 2
-    if (party === null) {
-      //split-by-response
-      const widthToBeDistributed = layout.vizWidth - (responses.length - 1) * layout.responseGap
-      responses.forEach((response, responseIdx) => {
-        const topLeftX = responses.slice(0, responseIdx).reduce((acc: number, curr) => acc + layout.responseGap + curr.proportion * widthToBeDistributed, 0)
-        const width = response.proportion * widthToBeDistributed;
-        const positions = pointPositions(topLeftX, topLeftY, width, segmentHeight, response.count, layout.pointRadius);
-        (response as ResponseWithSegment).segment = {
-          topLeftX: topLeftX,
-          topLeftY: topLeftY,
-          width: width,
-          height: segmentHeight,
-          pointPositions: positions.data
-        }
-        if (positions.error) {
-          console.warn("Point radius is too large for the segment at", wave, party, response.response)
-        }
-      })
-      return responses as ResponseWithSegment[]
-    } else {
-      //split-by-response-and-party
-      const partyTotalWidth = (layout.vizWidth - (partyResponseGroups.length - 1) * layout.partyGap) / partyResponseGroups.length
-      const widthToBeDistributed = partyTotalWidth - (responses.length - 1) * layout.responseGap
-      const foundPartyIdx = partyResponseGroups.findIndex((pg) => pg.join("|") === party.join("|"))
-      const partyIdx = foundPartyIdx >= 0 ? foundPartyIdx : 0
-      const partyTopLeftX = 
-    }
-  } else {
-    //split-by-wave
-    if (party === null) {
-      //split-by-response-and-wave
-    } else {
-      //split-by-response-and-wave-and-party
-    }
-  }
-
+  const group = segmentGroup(
+    wave,
+    party,
+    responses.length,
+    layout,
+    numWaves,
+    partyResponseGroups.length
+  )
+  const responsesWithSegments = segments(
+    responses,
+    group,
+    layout.responseGap,
+    layout.pointRadius
+  )
+  return responsesWithSegments
 }
--- src/plugins/build-data/index.ts
+++ src/plugins/build-data/index.ts
@@ -12,12 +12,48 @@ import pm from "../../assets/raw-data/meta-perf.json";
 import pi from "../../assets/raw-data/meta-imp.json";
 const metaPerf = pm as Meta;
 const metaImp = pi as Meta;
+//load the addCounts function
+import { addCounts } from "./functions/add-counts";
+import { addSegments } from "./functions/add-segments";
 
 export default function buildData() {
-  return ({
-    segments: {
-      imp: makeSegments(vizConfig, metaImp),
-      perf: makeSegments(vizConfig, metaPerf)
-    }
+  //construct the segments for each layout
+  vizConfig.layouts.map((layout) => {
+    /*  
+    This is almost there, but not quite.
+
+    addCounts and addSegments mutate the objects passed to them.
+    But we need separate arrays of splits-with-segments FOR EACH layout.
+
+    So I think we need to change addCounts and addSegments so that they
+    do NOT mutate the underlying meta objects and instead return
+    copies of those objects enhance with the segments.
+    */
+    metaPerf.response.characteristics.map((characteristic) => {
+      characteristic.splits.map((split) => {
+        if (split.responses) {
+          //add the counts
+          const expandedWithCounts = addCounts(split.responses.expanded, vizConfig.sample_size)
+          const collapsedWithCounts = addCounts(split.responses.collapsed, vizConfig.sample_size)
+          //add the segments
+          const expandedWithSegments = addSegments(
+            split.wave,
+            split.party,
+            expandedWithCounts,
+            layout,
+            metaPerf.wave.vals.length,
+            metaPerf.pid3.response_groups
+          )
+          const collapsedWithSegments = addSegments(
+            split.wave,
+            split.party,
+            collapsedWithCounts,
+            layout,
+            metaPerf.wave.vals.length,
+            metaPerf.pid3.response_groups
+          )
+        }
+      })
+    })
   })
 }
