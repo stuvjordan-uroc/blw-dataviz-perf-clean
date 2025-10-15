0af9496c1bc9db417228b285763114c0526d7e7a
stu <stuvjordan@gmail.com>
2025-10-15 14:31:10 -0400

    new version of buildData that writes a separate file for each characteristic-layout is drafted.  Have not tried to run it yet.

--- src/plugins/build-data/index.ts
+++ src/plugins/build-data/index.ts
@@ -13,6 +13,7 @@ const metaImp = pi as Meta;
 import { getZipFileNames } from "./functions/get-zip-file-names";
 import fs from 'node:fs'
 import path from 'node:path'
+import zlib from 'node:zlib'
 const rawDataPath = path.join(process.cwd(), 'src/assets/raw-data');
 const [charZipPathImp, charZipPathPerf] = [
   path.join(rawDataPath, 'characteristics-imp.zip'),
@@ -55,74 +56,63 @@ function runtimeData(
   meta: Meta,
   charZip: string,
   charFileNames: string[],
-  vizConfig: VizConfig
+  vizConfig: VizConfig,
+  outdir: string
 ) {
 
-  charFileNames.map((cfn) => {
+  charFileNames.forEach((cfn) => {
     // Read the contents of the file with name cfn from the zip archive at charZip
     // without reading any other files in the archive into memory
-    const charSplits = JSON.parse(extractZipFile(charZip, cfn)
-      .toString('utf8')) as Characteristic;
+    const characteristic = JSON.parse(
+      extractZipFile(charZip, cfn).toString('utf8')
+    ) as Characteristic;
+    const inWaveDates = meta.wave.dates
+      .filter((waveDate) => characteristic.in_waves.includes(waveDate[0]))
+      .sort((a, b) => a[0] - b[0])
+    const charName = characteristic.characteristic_name
+    // Create a new directory at the path outdir + '/' + charName
+    const charDir = outdir + '/' + charName;
+    fs.mkdirSync(charDir, { recursive: true });
+    vizConfig.layouts.forEach((layout) => {
+      const splits = {
+        splits: characteristic.splits
+          .map((split) => addCounts(
+            split,
+            vizConfig.sample_size,
+            meta.pid3.response_groups.length,
+            characteristic.in_waves.length
+          ))
+          .map((splitWithCount) => addSegments(
+            splitWithCount,
+            layout,
+            meta.pid3.response_groups,
+            characteristic.in_waves.length
+          )),
+        unsplitPositions: pointPositions(
+          0,
+          layout.labelHeight,
+          layout.vizWidth,
+          (layout.labelHeight + layout.waveHeight) * meta.wave.vals.length,
+          vizConfig.sample_size * meta.pid3.response_groups.length * characteristic.in_waves.length,
+          layout.pointRadius
+        )
+      }
 
+      // Create a gzipped JSON file for this layout
+      const fileName = layout.breakpoint + '.gz';
+      const filePath = charDir + '/' + fileName;
+      const jsonString = JSON.stringify({
+        ...splits,
+        waves: inWaveDates
+      });
+      const gzippedData = zlib.gzipSync(jsonString);
+      fs.writeFileSync(filePath, gzippedData);
 
-
-
-
-  })  // return meta.response.characteristics.map((characteristic) => {
-  //   const inWaveDates = meta.wave.dates
-  //     .filter((waveDate) => characteristic.in_waves.includes(waveDate[0]))
-  //     .sort((a, b) => a[0] - b[0])
-  //   return ({
-  //     charName: characteristic.characteristic_name,
-  //     inWaves: inWaveDates,
-  //     layouts: (Object.fromEntries(vizConfig.layouts.map((layout) => ([
-  //       layout.breakpoint,
-  //       {
-  //         splits: characteristic.splits
-  //           .map((split) => addCounts(
-  //             split,
-  //             vizConfig.sample_size,
-  //             meta.pid3.response_groups.length,
-  //             characteristic.in_waves.length
-  //           ))
-  //           .map((splitWithCount) => addSegments(
-  //             splitWithCount,
-  //             layout,
-  //             meta.pid3.response_groups,
-  //             characteristic.in_waves.length
-  //           )),
-  //         unsplitPositions: pointPositions(
-  //           0,
-  //           layout.labelHeight,
-  //           layout.vizWidth,
-  //           (layout.labelHeight + layout.waveHeight) * meta.wave.vals.length,
-  //           vizConfig.sample_size * meta.pid3.response_groups.length * characteristic.in_waves.length,
-  //           layout.pointRadius
-  //         )
-  //       }
-  //     ]))))
-  //   } as CharData)
-  // })
-}
-
-function writeRuntimeData(dirPathString: string, data: CharData[]): void {
-  fs.mkdirSync(dirPathString)
-  data.forEach((chardata) => {
-    const charDirPath = dirPathString + '/' + chardata.charName;
-    fs.mkdirSync(charDirPath, { recursive: true });
-    Object.entries(chardata.layouts).forEach(([breakPoint, splitData]) => {
-      const js = JSON.stringify(splitData)
-      fs.writeFileSync(charDirPath + '/' + breakPoint + '.json', js)
     })
-    fs.writeFileSync(charDirPath + '/' + 'meta.json', JSON.stringify({
-      waveData: chardata.inWaves
-    }))
   })
 }
 
 export default function buildData(): void {
-  const segmentsPerf = runtimeData(metaPerf, charZipPathPerf, charZipFileNamesPerf, vizConfig)
-  const segmentsImp = runtimeData(metaImp, charZipPathImp, charZipFileNamesImp, vizConfig)
-  writeRuntimeData('./public/perf', segmentsPerf)
-  writeRuntimeData('./public/imp', segmentsImp)
+  runtimeData(metaPerf, charZipPathPerf, charZipFileNamesPerf, vizConfig, './public/perf')
+  runtimeData(metaImp, charZipPathImp, charZipFileNamesImp, vizConfig, './public/imp')
 }
